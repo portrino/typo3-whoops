60d284cb81d333eba8dd2cb67592fbd02d1d86f2
a-wuttig <vagrant@unassigned-hostname.unassigned-domain>
2017-11-12 16:49:33 +0100

    [TASK] Uses php builtin server instead of apache for travis build

--- .travis/typo3/AdditionalConfiguration.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['debugExceptionHandler'] = \Portrino\Typo3Whoops\Error\WhoopsExceptionHandler::class;
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['productionExceptionHandler'] = \Portrino\Typo3Whoops\Error\WhoopsExceptionHandler::class;
